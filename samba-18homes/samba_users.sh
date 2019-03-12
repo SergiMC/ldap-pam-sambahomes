0ab9b5223e94065bff884f50562bbf1730aa0cd5
SergiMC <sergimuca59@gmail.com>
2019-03-12 12:28:53 +0100

    Update samba_users.sh

--- samba-18homes/samba_users.sh
+++ samba-18homes/samba_users.sh
@@ -8,12 +8,12 @@ useradd pla
 useradd patipla
 useradd rock
 
-echo -e "smbpla\nsmbpla" | smbpasswd -a pla
-echo -e "smbpatipla\nsmbpatipla" | smbpasswd -a patipla
-echo -e "smbrock\nsmbrock" | smbpasswd -a rock
-echo -e "smblila\nsmblila" | smbpasswd -a lila
-echo -e "smbpere\nsmbpere" | smbpasswd -a pere
-echo -e "smbpau\nsmbpau" | smbpasswd -a pau
-echo -e "smbmarta\nsmbmarta" | smbpasswd -a marta
+echo -e "pla\npla" | smbpasswd -a pla
+echo -e "patipla\npatipla" | smbpasswd -a patipla
+echo -e "rock\nrock" | smbpasswd -a rock
+echo -e "lila\nlila" | smbpasswd -a lila
+echo -e "pere\npere" | smbpasswd -a pere
+echo -e "smbpau\nsmbpau" | passwd -a pau
+echo -e "marta\nmarta" | smbpasswd -a marta
 
 
